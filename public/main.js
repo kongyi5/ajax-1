8e81e978703b9e09d0e0cecb5b656e2e0c395d56
kongyi <jerrykongyi1997@gmail.com>
2020-08-03 10:56:00 +0800

    监听readyState

--- public/main.js
+++ public/main.js
@@ -1,29 +1,38 @@
 getHTML.onclick = () => {
-  const request = new XMLHttpRequest();
-  request.open("GET", "/3.html");
-  request.onload = () => {
-    // console.log(request.response);
-
-    const div = document.createElement("div");
-    div.innerHTML = request.response;
-    document.body.appendChild(div);
+  const request = new XMLHttpRequest(); // readyState 0
+  request.open("GET", "/3.html"); // readyState 1
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const div = document.createElement("div");
+        div.innerHTML = request.response;
+        document.body.appendChild(div);
+      } else {
+        console.log(request.status);
+        alert("加载 html 失败");
+      }
+    }
   };
-  request.onerror = () => {};
-  request.send();
+  request.send(); // readyState 2
 };
 
 getCSS.onclick = () => {
   const request = new XMLHttpRequest();
   request.open("GET", "/style.css");
-  request.onload = () => {
-    // 创建 style 标签
-    const style = document.createElement("style");
-    // 填写 style 内容
-    style.innerHTML = request.response;
-    // 插到 head 里面
-    document.head.appendChild(style);
+  request.onreadystatechange = () => {
+    console.log(request.readyState);
+    if (request.readyState === 4) {
+      console.log("下载完成");
+      if (request.status >= 200 && request.status < 300) {
+        const style = document.createElement("style");
+        style.innerHTML = request.response;
+        document.head.appendChild(style);
+      } else {
+        console.log(request.status);
+        alert("加载 css 失败");
+      }
+    }
   };
-  request.onerror = () => {};
   request.send();
 };
 
